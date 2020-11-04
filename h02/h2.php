249ac55305d64f8e6e99ff121d1733ce1dbb9676
YousefAbood <yalgeefary@roc-dev.com>
2020-11-04 18:48:50 +0100

    Added variables & Constructed quotes using these variables

--- h02/h2.php
+++ h02/h2.php
@@ -1,2 +1,27 @@
 <?php
-echo 'This is a test';
+$a = "Ik heb niet gefaald";
+$b = "die niet werken";
+$c = 10000;
+
+echo $a . ". Ik heb alleen " . $c . " manieren gevonden " . $b . "." . "<br>";
+
+
+$d = "die geen fout";
+$e = "en";
+$f = "niet";
+
+echo "Mens" . $e . " " . $d . " maken, " . "werk" . $e . " ". $f . "." . "<br>";
+
+$g = "ry";
+$h = ", but";
+$i = "fail";
+
+echo "T" . $g . " and " . $i . $h . " never " . $i . " to " . "t" . $g . "." . "<br>";
+
+$j = "omgaan";
+$k = "met";
+$l = "teleu";
+
+echo "De cursus " . '"' . $j . " met " . $l . "rstellingen" . '"' . " kan vanavond helaas niet doorgaan.";
+
+
